8564d2367ebf8a54ab9dd1aed1ffc8aa2fef4cf5
Miguel Villamizar <logimpunk10@hotmail.com>
2025-08-26 09:32:31 -0500

    mvillamizar - 26/08/2025 - Ad photos, order videos, add new video(s), Fix Menu

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,18 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtp.gmail.com',
+    'username' => 'miguelvillamizar365@gmail.com',
+    'password' => '107096686310Mi*',
+    'port' => '465'
   );
-  */
+  
+echo $_POST['name'];
+echo $_POST['email'];
+echo $_POST['message'];
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
